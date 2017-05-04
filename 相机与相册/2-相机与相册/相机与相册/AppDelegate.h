3b2f08317774ad02540a7d7c12055710ae004871
黄永锐 <huangyongrui@huangyongruideiMac.local>
2017-05-04 09:49:29 +0800

    相机与相册拍照保存完毕可以返回

---
+++ 相机与相册/2-相机与相册/相机与相册/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  相机与相册
+//
+//  Created by 黄永锐 on 2017/5/3.
+//  Copyright © 2017年 LoveQi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 相机与相册/2-相机与相册/相机与相册/MyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyTableViewController.h
+//  相机与相册
+//
+//  Created by 黄永锐 on 2017/5/3.
+//  Copyright © 2017年 LoveQi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewController : UITableViewController
+
+@end
